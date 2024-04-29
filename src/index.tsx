9f80525b15caeb79896905027e87a4ecc0adbe4d
Marko Lieposhkin <marko.lieposhkin-ext@keolis.com>
2024-04-29 14:25:37 +0300

    ton ui provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import App from './App';
 
 
@@ -9,6 +9,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <TonConnectUIProvider manifestUrl="https://musical-licorice-e034aa.netlify.app/tonconnect-manifest.json">
     <App />
+    </TonConnectUIProvider>
   </React.StrictMode>
 );
