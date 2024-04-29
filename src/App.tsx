26c1838c0341976a8c581d2bd2b5cafceb44cbe3
Marko Lieposhkin <marko.lieposhkin-ext@keolis.com>
2024-04-29 13:42:20 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+const tg = window.Telegram.WebApp;
+
 function App() {
+  useEffect(() => {
+
+    tg.ready();
+  }, []);
+  const onClose = () => {
+    tg.close();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClose}>Hello!</button>
     </div>
   );
 }
