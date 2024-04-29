f03fa8f2f6f8228624066b8503940f786ca82a6e
Marko Lieposhkin <marko.lieposhkin-ext@keolis.com>
2024-04-29 16:09:07 +0300

    Transaction implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import { useEffect } from 'react';
-import './App.css';
-import { TonConnectButton, useTonWallet } from '@tonconnect/ui-react';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { TonConnectButton, useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
 
 const tg = window.Telegram.WebApp;
 
 function App() {
-  const wallet = useTonWallet();
-  
-  useEffect(() => {
+  const [tonAdress, setTonAdress] = useState("");
+  const [tonAmount, setTonAmount] = useState("");
+  const [tonConnectUI] = useTonConnectUI();
 
+  const wallet = useTonWallet();
+
+  useEffect(() => {
     tg.ready();
   }, []);
+
   const onClose = () => {
     tg.close();
+  };
+
+  const sendTonTransaction = async (event: React.FormEvent) => {
+  event.preventDefault();
+    const transaction = {
+      messages: [
+          {
+              address: tonAdress,
+              amount: tonAmount,
+            }
+          ],
+          validUntil: Math.floor(Date.now() / 1000) + 1 * 60
   }
-  console.log(wallet);
+
+  tonConnectUI.sendTransaction(transaction)
+  }
+  
   return (
     <div className="App">
       <TonConnectButton />
-      {wallet ? <div>Wallet succesfully connected: {wallet.account.address}</div>: null }
+      {wallet ? (
+        <>
+          <form onSubmit={sendTonTransaction}>
+          <input placeholder="Input TON amount" type='number' onChange={e => setTonAmount(e.target.value)}></input>
+          <input placeholder="Input TON address" type='text' onChange={e => setTonAdress(e.target.value)}></input>
+          {tonAdress && tonAmount ? <button type="submit">Send TON</button> : null}
+          </form>
+        </>
+      ) : null}
       <button onClick={onClose}>Close app</button>
     </div>
   );
