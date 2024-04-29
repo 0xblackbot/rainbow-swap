c39074e6fe92e707afbb562b9039668ee1a7eec2
Marko Lieposhkin <marko.lieposhkin-ext@keolis.com>
2024-04-29 14:06:37 +0300

    5

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   }
   return (
     <div className="App">
-      <button onClick={onClose}>Hello!</button>
+      <button onClick={onClose}>Close app</button>
     </div>
   );
 }
