980d579d6c70e4ad632ba102fc2b06ce3e06a03a
Marko Lieposhkin <marko.lieposhkin-ext@keolis.com>
2024-04-29 14:48:42 +0300

    123

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
 import './App.css';
+import { TonConnectButton, useTonWallet } from '@tonconnect/ui-react';
 
 const tg = window.Telegram.WebApp;
 
 function App() {
+  const wallet = useTonWallet();
+  
   useEffect(() => {
 
     tg.ready();
@@ -11,8 +14,11 @@ function App() {
   const onClose = () => {
     tg.close();
   }
+  console.log(wallet);
   return (
     <div className="App">
+      <TonConnectButton />
+      {wallet ? <div>Wallet succesfully connected: {wallet.account.address}</div>: null }
       <button onClick={onClose}>Close app</button>
     </div>
   );
