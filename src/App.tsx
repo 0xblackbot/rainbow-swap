f6a06db60b614e6b272f0e81ba1a1797039ac586
Andrii Ivaniv <unixvb@gmail.com>
2024-04-29 16:47:57 +0300

    eslint+prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,69 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { TonConnectButton, useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
+import {
+    TonConnectButton,
+    useTonConnectUI,
+    useTonWallet
+} from '@tonconnect/ui-react';
+import {useEffect, useState} from 'react';
+
+import './App.css';
 
 const tg = window.Telegram.WebApp;
 
 function App() {
-  const [tonAdress, setTonAdress] = useState("");
-  const [tonAmount, setTonAmount] = useState("");
-  const [tonConnectUI] = useTonConnectUI();
+    const [tonAddress, setTonAddress] = useState('');
+    const [tonAmount, setTonAmount] = useState('');
+    const [tonConnectUI] = useTonConnectUI();
 
-  const wallet = useTonWallet();
+    const wallet = useTonWallet();
 
-  useEffect(() => {
-    tg.ready();
-  }, []);
+    useEffect(() => {
+        tg.ready();
+    }, []);
 
-  const onClose = () => {
-    tg.close();
-  };
+    const onClose = () => {
+        tg.close();
+    };
 
-  const sendTonTransaction = async (event: React.FormEvent) => {
-  event.preventDefault();
-    const transaction = {
-      messages: [
-          {
-              address: tonAdress,
-              amount: tonAmount,
-            }
-          ],
-          validUntil: Math.floor(Date.now() / 1000) + 1 * 60
-  }
+    const sendTonTransaction = async (event: React.FormEvent) => {
+        event.preventDefault();
+        const transaction = {
+            messages: [
+                {
+                    address: tonAddress,
+                    amount: tonAmount
+                }
+            ],
+            validUntil: Math.floor(Date.now() / 1000) + 1 * 60
+        };
 
-  tonConnectUI.sendTransaction(transaction)
-  }
-  
-  return (
-    <div className="App">
-      <TonConnectButton />
-      {wallet ? (
-        <>
-          <form onSubmit={sendTonTransaction}>
-          <input placeholder="Input TON amount" type='number' onChange={e => setTonAmount(e.target.value)}></input>
-          <input placeholder="Input TON address" type='text' onChange={e => setTonAdress(e.target.value)}></input>
-          {tonAdress && tonAmount ? <button type="submit">Send TON</button> : null}
-          </form>
-        </>
-      ) : null}
-      <button onClick={onClose}>Close app</button>
-    </div>
-  );
+        tonConnectUI.sendTransaction(transaction);
+    };
+
+    return (
+        <div className="App">
+            <TonConnectButton />
+            {wallet ? (
+                <>
+                    <form onSubmit={sendTonTransaction}>
+                        <input
+                            placeholder="Input TON amount"
+                            type="number"
+                            onChange={e => setTonAmount(e.target.value)}
+                        ></input>
+                        <input
+                            placeholder="Input TON address"
+                            type="text"
+                            onChange={e => setTonAddress(e.target.value)}
+                        ></input>
+                        {tonAddress && tonAmount ? (
+                            <button type="submit">Send TON</button>
+                        ) : null}
+                    </form>
+                </>
+            ) : null}
+            <button onClick={onClose}>Close app</button>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
+import {TonConnectUIProvider} from '@tonconnect/ui-react';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import {createRoot} from 'react-dom/client';
+
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <React.StrictMode>
-    <TonConnectUIProvider manifestUrl="https://musical-licorice-e034aa.netlify.app/tonconnect-manifest.json">
-    <App />
-    </TonConnectUIProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <TonConnectUIProvider manifestUrl="https://musical-licorice-e034aa.netlify.app/tonconnect-manifest.json">
+            <App />
+        </TonConnectUIProvider>
+    </React.StrictMode>
 );
