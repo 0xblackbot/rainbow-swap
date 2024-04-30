caa632f5d0ba151a3202838dfd51dbecc63d9e0e
Andrii Ivaniv <unixvb@gmail.com>
2024-04-30 12:10:39 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import './App.css';
 
 const tg = window.Telegram.WebApp;
 
+// test commit
 function App() {
     const [tonAddress, setTonAddress] = useState('');
     const [tonAmount, setTonAmount] = useState('');
