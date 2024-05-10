eb6024da3f8a77ed84c86e57d7d1ee9b289d5b82
Andrii Ivaniv <unixvb@gmail.com>
2024-05-10 19:09:30 +0300

    base url fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {nodePolyfills} from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [
         react(),
         nodePolyfills({
