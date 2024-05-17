71c0e3a7fb35acee8158868b5a8e4473f04e4cfb
Andrii Ivaniv <unixvb@gmail.com>
2024-05-17 13:21:47 +0300

    api url changed

--- src/globals.ts
+++ src/globals.ts
@@ -14,7 +14,7 @@ export const GAS_AMOUNT = toNano('0.255', 9);
 export const JETTON_TRANSFER_GAS_AMOUNT = toNano('0.065', 9);
 
 export const API = axios.create({
-    baseURL: 'https://api.blackbot.technology/api'
+    baseURL: 'https://api.blackbot.technology/rainbow/api'
 });
 export const TON_CLIENT = new TonClient({
     endpoint: `https://scalable-api.tonwhales.com/jsonRPC`
