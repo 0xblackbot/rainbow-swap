5e409f298c71e15e3c0482e5c1e529fdcd736c43
LMykhailo <jenevven@gmail.com>
2024-05-24 11:02:20 +0300

    Fixed opacity

--- src/shared/asset-selector/sheet-backdrop/sheet-backdrop.tsx
+++ src/shared/asset-selector/sheet-backdrop/sheet-backdrop.tsx
@@ -34,7 +34,7 @@ export const SheetBackdrop = forwardRef<any, SheetBackdropProps>(
                 className={`react-modal-sheet-backdrop ${className}`}
                 style={{...styles.backdrop, ...style, pointerEvents: 'auto'}}
                 initial={{opacity: 0}}
-                animate={{opacity: 0.7}}
+                animate={{opacity: 0.6}}
                 exit={{opacity: 0}}
                 transition={{duration: 0.3}}
             />
