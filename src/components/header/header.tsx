3935306fd05e7017e93e453c62ea25d3cac17146
LMykhailo <jenevven@gmail.com>
2024-05-30 13:21:57 +0300

    Disabling main button on tonConnectUI modal open

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import {useTonAddress, useTonConnectModal} from '@tonconnect/ui-react';
+import {useEffect} from 'react';
 
 import {LogoText} from './assets/LogoText';
 import styles from './header.module.css';
@@ -6,11 +7,31 @@ import {PendingSwap} from './pending-swap/pending-swap.tsx';
 import {WalletMenu} from './wallet-menu/wallet-menu.tsx';
 import {HeaderContainer} from '../header-container/header-container.tsx';
 
+const DEFAULT_HINT_COLOR = '#3e3e42';
+
 export const Header = () => {
     const walletAddress = useTonAddress();
     const connectModal = useTonConnectModal();
 
-    const handleConnect = () => connectModal.open();
+    const handleConnect = () => {
+        connectModal.open();
+    };
+
+    useEffect(() => {
+        if (connectModal.state.status === 'opened') {
+            const prevMainButtonColor = window.Telegram.WebApp.MainButton.color;
+
+            window.Telegram.WebApp.MainButton.disable();
+            window.Telegram.WebApp.MainButton.color =
+                window.Telegram.WebApp.themeParams.hint_color ??
+                DEFAULT_HINT_COLOR;
+
+            return () => {
+                window.Telegram.WebApp.MainButton.enable();
+                window.Telegram.WebApp.MainButton.color = prevMainButtonColor;
+            };
+        }
+    }, [connectModal.state.status]);
 
     return (
         <HeaderContainer>
