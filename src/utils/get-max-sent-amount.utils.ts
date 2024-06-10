02736e56875138af3c42a05c4e282e948934f9b1
LMykhailo <jenevven@gmail.com>
2024-06-10 09:59:33 +0300

    Fixed PR comment

--- src/utils/get-max-sent-amount.utils.ts
+++ src/utils/get-max-sent-amount.utils.ts
@@ -1,15 +1,17 @@
 import {fromNano} from './big-int.utils';
 import {GAS_AMOUNT, JETTON_TRANSFER_GAS_AMOUNT, TON_DECIMALS} from '../globals';
 
+const MAX_BATCH_SIZE = 4;
+
 export const getMaxSentAmount = (balance: string) => {
     const gasAmount = GAS_AMOUNT;
     const jettonGasAmount = 3n * JETTON_TRANSFER_GAS_AMOUNT;
     const totalGasAmount = gasAmount + jettonGasAmount;
 
-    //Or should we multiply it by 4 as well because we have 4 possible routes?
     const tonBalanceWithFee =
-        parseFloat(balance) -
-        parseFloat(fromNano(totalGasAmount, TON_DECIMALS));
+        (parseFloat(balance) -
+            parseFloat(fromNano(totalGasAmount, TON_DECIMALS))) *
+        MAX_BATCH_SIZE;
 
     const tonBalance = Math.max(tonBalanceWithFee, 0);
     return tonBalance.toFixed(TON_DECIMALS);
