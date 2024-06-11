be835f04290534fbb6252c9b063963cf394d0d26
Andrii Ivaniv <unixvb@gmail.com>
2024-06-11 14:01:33 +0300

    Contract activation text improved

--- src/screens/home/swap-route-info/rainbow-wallet-info/rainbow-wallet-info.tsx
+++ src/screens/home/swap-route-info/rainbow-wallet-info/rainbow-wallet-info.tsx
@@ -3,20 +3,17 @@ import {FC} from 'react';
 import styles from './rainbow-wallet-info.module.css';
 import {InfoIcon} from '../../../../assets/icons/InfoIcon/InfoIcon.tsx';
 
-export const RainbowWalletInfo: FC = () => {
-    return (
-        <>
-            <div className={styles.container}>
-                <div className={styles.header}>
-                    <InfoIcon className={styles.route_info_header_logo} />
-                    <p className={styles.header_text}>Action needed</p>
-                </div>
-                <p className={styles.text}>
-                    Before your first swap on our DEX, you'll need to deploy
-                    your own smart contract. This is a one-time transaction.
-                    After that, you can proceed with your swap.
-                </p>
+export const RainbowWalletInfo: FC = () => (
+    <>
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <InfoIcon className={styles.route_info_header_logo} />
+                <p className={styles.header_text}>Action needed</p>
             </div>
-        </>
-    );
-};
+            <p className={styles.text}>
+                Press 'Activate contract' to deploy your smart contract. It's a
+                one-time action. Once done, you can continue with your swap.
+            </p>
+        </div>
+    </>
+);
