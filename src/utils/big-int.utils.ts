dda22357abc6668db2e1cf9c5790b1d443b05667
Andrii Ivaniv <unixvb@gmail.com>
2024-06-19 10:12:11 +0300

    toNano fix

--- src/utils/big-int.utils.ts
+++ src/utils/big-int.utils.ts
@@ -1,4 +1,42 @@
-export {toNano} from '@ton/core';
+export const toNano = (src: string, decimals: number) => {
+    const precision = 10n * BigInt(decimals);
+
+    // Check sign
+    let neg = false;
+    while (src.startsWith('-')) {
+        neg = !neg;
+        src = src.slice(1);
+    }
+    // Split string
+    if (src === '.') {
+        throw Error('Invalid number');
+    }
+    const parts = src.split('.');
+    if (parts.length > 2) {
+        throw Error('Invalid number');
+    }
+    // Prepare parts
+    let whole = parts[0];
+    let frac = parts[1];
+    if (!whole) {
+        whole = '0';
+    }
+    if (!frac) {
+        frac = '0';
+    }
+    if (frac.length > 9) {
+        throw Error('Invalid number');
+    }
+    while (frac.length < 9) {
+        frac += '0';
+    }
+    // Convert
+    let r = BigInt(whole) * precision + BigInt(frac);
+    if (neg) {
+        r = -r;
+    }
+    return r;
+};
 
 export const fromNano = (value: bigint, decimals: number) =>
     String(Number(value) / 10 ** decimals);
