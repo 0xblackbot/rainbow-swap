fc05bca64ed8e84c3891aa337b618456f2835f64
Andrii Ivaniv <unixvb@gmail.com>
2024-07-29 16:03:35 +0300

    coin tap ui added

--- src/components/points-modal/points-modal.tsx
+++ src/components/points-modal/points-modal.tsx
@@ -1,13 +1,44 @@
+import React, {useState} from 'react';
+
+import coinImage from './coin.png';
+import styles from './points-modal.module.css';
+import {Click} from '../../interfaces/click.interface';
 import {BottomSheet} from '../../shared/bottom-sheet/bottom-sheet';
 import {useDispatch} from '../../store';
 import {closePointsModal} from '../../store/points/points-actions';
 import {useIsPointsModalOpenSelector} from '../../store/points/points-selectors';
+import {getClassName} from '../../utils/style.utils';
 
 export const PointsModal = () => {
     const dispatch = useDispatch();
     const isOpen = useIsPointsModalOpenSelector();
 
+    const [isPressed, setIsPressed] = useState(false);
+    const [clicks, setClicks] = useState<Click[]>([]);
+
     const handleClose = () => dispatch(closePointsModal());
+    const handlePressStart = () => setIsPressed(true);
+    const handlePressEnd = () => setIsPressed(false);
+    const handleClick = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        window.Telegram.WebApp.HapticFeedback.impactOccurred('soft');
+
+        const rect = event.currentTarget.getBoundingClientRect();
+
+        setClicks(prevClicks =>
+            prevClicks.concat([
+                {
+                    id: Date.now(),
+                    x: event.clientX - rect.left,
+                    y: event.clientY - rect.top
+                }
+            ])
+        );
+    };
+    const handleAnimationEnd = (id: number) => {
+        setClicks(prevClicks => prevClicks.filter(click => click.id !== id));
+    };
 
     return (
         <BottomSheet
@@ -15,7 +46,39 @@ export const PointsModal = () => {
             headerTitle="Tap-tap"
             onClose={handleClose}
         >
-            <p>Hello hello</p>
+            <div
+                className={styles.coin_container}
+                onClick={handleClick}
+                onMouseDown={handlePressStart}
+                onMouseUp={handlePressEnd}
+                onMouseLeave={handlePressEnd}
+                onTouchStart={handlePressStart}
+                onTouchEnd={handlePressEnd}
+                onTouchCancel={handlePressEnd}
+            >
+                <img
+                    src={coinImage}
+                    alt="coin"
+                    draggable={false}
+                    className={getClassName(
+                        styles.coin_image,
+                        isPressed ? styles.coin_image_pressed : ''
+                    )}
+                />
+                {clicks.map(click => (
+                    <div
+                        key={click.id}
+                        className={styles.click}
+                        style={{
+                            top: click.y,
+                            left: click.x
+                        }}
+                        onAnimationEnd={() => handleAnimationEnd(click.id)}
+                    >
+                        +1
+                    </div>
+                ))}
+            </div>
         </BottomSheet>
     );
 };
---
+++ src/interfaces/click.interface.ts
@@ -0,0 +1,5 @@
+export interface Click {
+    id: number;
+    x: number;
+    y: number;
+}
