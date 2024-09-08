8ade44375e064884f38ca815b06797eadb1b3c28
Andrii Ivaniv <unixvb@gmail.com>
2024-09-08 13:15:38 +0300

    route info css fixed

--- src/components/swap-form/swap-details/swap-route-info/swap-route-info.tsx
+++ src/components/swap-form/swap-details/swap-route-info/swap-route-info.tsx
@@ -46,7 +46,7 @@ export const SwapRouteInfo: FC<Props> = ({swapInfo}) => {
             >
                 <p>You send</p>
                 <div>
-                    <p>
+                    <p className={styles.value_text}>
                         {swapInfo.inputAssetAmount} {inputAsset.symbol}
                     </p>
                     <img
@@ -63,7 +63,7 @@ export const SwapRouteInfo: FC<Props> = ({swapInfo}) => {
             >
                 <p>You receive</p>
                 <div>
-                    <p>
+                    <p className={styles.value_text}>
                         {swapInfo.outputAssetAmount} {outputAsset.symbol}
                     </p>
                     <img
@@ -74,21 +74,26 @@ export const SwapRouteInfo: FC<Props> = ({swapInfo}) => {
             </div>
             <div className={styles.route_info_inside_div}>
                 <p>Rate</p>
-                <p>
+                <p className={styles.value_text}>
                     {`1 ${inputAsset.symbol} = ${formatNumber(swapInfo.exchangeRate, 5)} ${outputAsset.symbol}`}
                 </p>
             </div>
             <div className={styles.route_info_inside_div}>
                 <p>Fee</p>
-                <p>0%</p>
+                <p className={styles.value_text}>
+                    <span className={styles.crossed_out}>0.1%</span>
+                    <span className={styles.fee_text}> 0%</span>
+                </p>
             </div>
             <div className={styles.route_info_inside_div}>
                 <p>Max. slippage</p>
-                <p>{slippageTolerance}%</p>
+                <p className={styles.value_text}>{slippageTolerance}%</p>
             </div>
             <div className={styles.route_info_inside_div}>
                 <p>Receive at least</p>
-                <p>{`${swapInfo.minOutputAssetAmount} ${outputAsset.symbol}`}</p>
+                <p
+                    className={styles.value_text}
+                >{`${swapInfo.minOutputAssetAmount} ${outputAsset.symbol}`}</p>
             </div>
             <div className={styles.route_info_inside_div}>
                 <p>Swap route</p>
