9c5dd0925e0d30e5556c3ac0d75be803d4600ad5
Andrii Ivaniv <unixvb@gmail.com>
2024-09-12 14:28:34 +0300

    footer fixed

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,45 +1,33 @@
 import styles from './footer.module.css';
 import {COMMUNITY_CHAT_LINK, TELEGRAM_CHANNEL_LINK} from '../../globals';
-import {useViewportHeight} from '../../hooks/viewport-height/viewport-height.hook';
 import {getClassName} from '../../utils/style.utils';
 
-const CONTAINER_HEIGHT = 40;
-const CONTAINER_MARGIN_BOTTOM = 20;
-
-export const Footer = () => {
-    const viewportHeight = useViewportHeight();
-    const top =
-        viewportHeight.value - (CONTAINER_HEIGHT + CONTAINER_MARGIN_BOTTOM);
-
-    return (
-        <div className={styles.container} style={{top}}>
-            <div className={styles.container_row}>
-                <a
-                    className={getClassName(
-                        styles.container_text,
-                        styles.container_a
-                    )}
-                    href={TELEGRAM_CHANNEL_LINK}
-                    target="_blank"
-                >
-                    Telegram Channel
-                </a>
-                <a
-                    className={getClassName(
-                        styles.container_text,
-                        styles.container_a
-                    )}
-                    href={COMMUNITY_CHAT_LINK}
-                    target="_blank"
-                >
-                    Community Chat
-                </a>
-            </div>
-            <div className={styles.container_row}>
-                <p className={styles.container_text}>
-                    Copyright © 2024 Blackbot
-                </p>
-            </div>
+export const Footer = () => (
+    <div className={styles.container}>
+        <div className={styles.container_row}>
+            <a
+                className={getClassName(
+                    styles.container_text,
+                    styles.container_a
+                )}
+                href={TELEGRAM_CHANNEL_LINK}
+                target="_blank"
+            >
+                Telegram Channel
+            </a>
+            <a
+                className={getClassName(
+                    styles.container_text,
+                    styles.container_a
+                )}
+                href={COMMUNITY_CHAT_LINK}
+                target="_blank"
+            >
+                Community Chat
+            </a>
         </div>
-    );
-};
+        <div className={styles.container_row}>
+            <p className={styles.container_text}>Copyright © 2024 Blackbot</p>
+        </div>
+    </div>
+);
