144fd80f9b7f236af305bc867f497e8a3fe6573d
Andrii Ivaniv <unixvb@gmail.com>
2024-09-14 12:23:36 +0300

    arbitrage mode added

--- src/components/swap-form/farm-volume/farm-volume.tsx
+++ src/components/swap-form/farm-volume/farm-volume.tsx
@@ -2,7 +2,7 @@ import Lottie from 'lottie-react';
 
 import duckAirdropAnimation from './duck-airdrop.json';
 import styles from './farm-volume.module.css';
-import {TON} from '../../../globals';
+import {TON, USDT} from '../../../globals';
 import {useSwapForm} from '../../../hooks/swap-form/swap-form.hook';
 
 const DELEN_BADGE_URL = 'https://society.ton.org/degen-airdrop';
@@ -14,7 +14,14 @@ export const FarmVolume = () => {
 
     const handleDegenBadgeClick = () =>
         window.Telegram.WebApp.openLink(DELEN_BADGE_URL);
+
     const handleFarmVolumeClick = () => {
+        setInputAssetAddress(TON);
+        setOutputAssetAddress(USDT);
+        setInputAssetAmount(TON_INPUT_AMOUNT);
+    };
+
+    const handleTryArbitrageModeClick = () => {
         setInputAssetAddress(TON);
         setOutputAssetAddress(TON);
         setInputAssetAmount(TON_INPUT_AMOUNT);
@@ -46,6 +53,15 @@ export const FarmVolume = () => {
                             onClick={handleFarmVolumeClick}
                         >
                             Farm trading volume
+                        </p>
+                    </div>
+                    <div className={styles.message_container}>
+                        <p className={styles.message}>3. </p>
+                        <p
+                            className={styles.button}
+                            onClick={handleTryArbitrageModeClick}
+                        >
+                            Try arbitrage mode
                         </p>
                     </div>
                 </div>
