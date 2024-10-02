f479412fc9e0986aead3c41b93970915d9f22a06
Andrii Ivaniv <unixvb@gmail.com>
2024-10-03 01:40:58 +0300

    buttons stack fix

--- src/shared/form-button/form-button.tsx
+++ src/shared/form-button/form-button.tsx
@@ -19,6 +19,7 @@ const updateMainButton = () => {
 
             window.Telegram.WebApp.MainButton.setText(lastButtonProps.text);
             window.Telegram.WebApp.MainButton.onClick(lastButtonProps.onClick);
+            window.Telegram.WebApp.MainButton.show();
         }
     }, 0);
 };
