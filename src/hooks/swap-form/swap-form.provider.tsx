dbbcdcf35dd146d045a1e016081146996e895d32
Andrii Ivaniv <unixvb@gmail.com>
2024-10-04 10:17:26 +0300

    react helmet added

--- src/hooks/swap-form/swap-form.provider.tsx
+++ src/hooks/swap-form/swap-form.provider.tsx
@@ -1,4 +1,5 @@
 import {FC, PropsWithChildren, useState} from 'react';
+import {Helmet} from 'react-helmet';
 
 import {SwapFormContext} from './swap-form.context';
 import {TON, USDT} from '../../globals';
@@ -35,6 +36,17 @@ export const SwapFormProvider: FC<PropsWithChildren> = ({children}) => {
                 outputAsset
             }}
         >
+            <Helmet>
+                <title>{`Swap ${inputAsset.symbol} to ${outputAsset.symbol} | Rainbow Swap`}</title>
+                <meta
+                    name="description"
+                    content={`Swap ${inputAsset.symbol} to ${outputAsset.symbol} at best rates and minimal price impact.`}
+                />
+                <link
+                    rel="canonical"
+                    href={`https://rainbow.ag/${inputAsset.slug}/${outputAsset.slug}`}
+                />
+            </Helmet>
             {children}
         </SwapFormContext.Provider>
     );
