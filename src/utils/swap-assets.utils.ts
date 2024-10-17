e3212b5a30423814c5e06ce1decfd83f6db58fe4
Andrii Ivaniv <unixvb@gmail.com>
2024-10-17 17:15:11 +0300

    Arbitrage mode disabled

--- src/utils/swap-assets.utils.ts
+++ src/utils/swap-assets.utils.ts
@@ -1,12 +1,10 @@
-import {TON} from '../globals';
-
 export const swapAssets = (
     newAssetAddress: string,
     otherAssetAddress: string,
     setCurrentAssetAddress: (address: string) => void,
     handleToggleAssetsClick: () => void
 ) => {
-    if (newAssetAddress === otherAssetAddress && newAssetAddress !== TON) {
+    if (newAssetAddress === otherAssetAddress) {
         handleToggleAssetsClick();
     } else {
         setCurrentAssetAddress(newAssetAddress);
