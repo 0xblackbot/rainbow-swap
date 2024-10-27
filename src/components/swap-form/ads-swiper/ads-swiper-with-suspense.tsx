49fc0785e28004f9e485039dafc84f18435194b2
Andrii Ivaniv <unixvb@gmail.com>
2024-10-27 17:41:32 +0200

    sleep removed

--- src/components/swap-form/ads-swiper/ads-swiper-with-suspense.tsx
+++ src/components/swap-form/ads-swiper/ads-swiper-with-suspense.tsx
@@ -1,10 +1,9 @@
-import {Suspense, lazy} from 'react';
+import {lazy, Suspense} from 'react';
 
 import styles from './ads-swiper.module.css';
-import {sleep} from '../../../utils/promise.utils';
 import {Skeleton} from '../../skeleton/skeleton';
 
-const AdsSwiper = lazy(() => sleep(5000).then(() => import('./ads-swiper')));
+const AdsSwiper = lazy(() => import('./ads-swiper'));
 
 export const AdsSwiperWithSuspense = () => (
     <Suspense
