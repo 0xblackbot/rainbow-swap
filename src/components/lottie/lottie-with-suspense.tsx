92e7b95fdc83bb7b6c53db998d005d93b572df41
Andrii Ivaniv <unixvb@gmail.com>
2024-10-28 09:50:20 +0200

    path fix

--- src/components/lottie/lottie-with-suspense.tsx
+++ src/components/lottie/lottie-with-suspense.tsx
@@ -1,12 +1,13 @@
 import {FC, Suspense, lazy} from 'react';
 
 import {LottieProps} from './lottie.props';
+import {BASE_URL} from '../../globals';
 import {Skeleton} from '../skeleton/skeleton';
 
 const Lottie = lazy(() => import('./lottie'));
 
 export const LottieWithSuspense: FC<LottieProps> = ({src, className}) => (
     <Suspense fallback={<Skeleton isLoading={true} className={className} />}>
-        <Lottie src={src} className={className} />
+        <Lottie src={BASE_URL + src} className={className} />
     </Suspense>
 );
