1519df9eb97811c37be8e6ee24a72a09974cae92
Andrii Ivaniv <unixvb@gmail.com>
2024-11-18 16:59:33 +0200

    sentry traces added

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import {sentryVitePlugin} from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
 import mkcert from 'vite-plugin-mkcert';
@@ -6,8 +7,17 @@ import Terminal from 'vite-plugin-terminal';
 // https://vitejs.dev/config/
 export default defineConfig({
     base: process.env.VITE_BASE_URL ?? '/',
-    plugins: [react(), mkcert({force: true}), Terminal()],
+    plugins: [
+        react(),
+        mkcert({force: true}),
+        Terminal(),
+        sentryVitePlugin({
+            org: 'rainbowag',
+            project: 'rainbow-ag'
+        })
+    ],
     build: {
-        minify: 'terser'
+        minify: 'terser',
+        sourcemap: true
     }
 });
