a4e7325a54b4466d448f12613e5364411e6e001f
Andrii Ivaniv <unixvb@gmail.com>
2024-11-27 12:49:24 +0200

    swaps enabled

--- src/components/swap-form/ads-swiper/farm-volume/farm-volume.tsx
+++ src/components/swap-form/ads-swiper/farm-volume/farm-volume.tsx
@@ -1,22 +1,51 @@
 import styles from './farm-volume.module.css';
+import {useSwapForm} from '../../../../contexts/swap-form/swap-form.hook';
+import {TON, USDT} from '../../../../globals';
 import {LottieWithSuspense} from '../../../lottie/lottie-with-suspense';
 
+const BADGE_URL = 'https://society.ton.org/the-open-league-new-year-airdrop';
+const TON_INPUT_AMOUNT = '20';
+
 export const FarmVolume = () => {
+    const {setInputAssetAddress, setOutputAssetAddress, setInputAssetAmount} =
+        useSwapForm();
+
+    const handleBadgeClick = () => window.Telegram.WebApp.openLink(BADGE_URL);
+
+    const handleFarmVolumeClick = () => {
+        setInputAssetAddress(TON);
+        setOutputAssetAddress(USDT);
+        setInputAssetAmount(TON_INPUT_AMOUNT);
+    };
+
     return (
         <div className={styles.container}>
-            <p className={styles.title}>
-                The Open League New Year Special <br />
-                Airdrop is coming!
-            </p>
+            <p className={styles.title}>Farm $1,500,000 Airdrop</p>
             <div className={styles.inner_container}>
                 <div className={styles.list_container}>
                     <div className={styles.message_container}>
-                        <p className={styles.button}>
-                            $1,500,000 airdrop for users
+                        <p className={styles.message}>1</p>
+                        <p className={styles.button} onClick={handleBadgeClick}>
+                            Get free badge
                         </p>
                     </div>
                     <div className={styles.message_container}>
-                        <p className={styles.button}>November 27th...</p>
+                        <p className={styles.message}>2</p>
+                        <p
+                            className={styles.button}
+                            onClick={handleFarmVolumeClick}
+                        >
+                            Swap any tokens
+                        </p>
+                    </div>
+                    <div className={styles.message_container}>
+                        <p className={styles.message}>3</p>
+                        <p
+                            className={styles.button}
+                            onClick={handleFarmVolumeClick}
+                        >
+                            More you swap - more you get
+                        </p>
                     </div>
                 </div>
 
