538d5ae92aee0c407d6a68a3bdb395020734102c
Andrii Ivaniv <unixvb@gmail.com>
2025-01-13 12:42:41 +0200

    old tasks removed

--- src/components/points-modal/social-tasks/social-tasks.tsx
+++ src/components/points-modal/social-tasks/social-tasks.tsx
@@ -1,21 +1,13 @@
 import {EmptyFn} from '@rnw-community/shared';
 import {FC} from 'react';
 
-import appsCenterImage from './assets/apps-center.jpg';
-import blockLabsImage from './assets/blockLabs.jpeg';
-import claytonImage from './assets/clayton.jpeg';
-import intractImage from './assets/intract.png';
 import jvaultImage from './assets/jvault.jpeg';
-import notPixelImage from './assets/notPixel.jpeg';
 import parratonImage from './assets/parraton.jpeg';
 import referralImage from './assets/referral.png';
-import snapxImage from './assets/snapx.jpg';
 import stakingImage from './assets/staking.png';
 import telegramImage from './assets/telegram.png';
-import terminalImage from './assets/terminal.jpeg';
 import tonAppImage from './assets/ton-app.png';
 import tonHedgeImage from './assets/ton-hedge.jpeg';
-import tonStationImage from './assets/tonStation.jpeg';
 import torchFinanceImage from './assets/torch-finance.jpeg';
 import twitterImage from './assets/twitter.png';
 import {Divider} from './divider/divider';
@@ -51,13 +43,6 @@ export const SocialTasks: FC<Props> = ({onSwap}) => {
                 </Button>
             </TaskItem>
 
-            <PartnerTaskItem
-                imageSrc={intractImage}
-                title="Hit $500+ Trading Volume"
-                taskType={TaskTypeEnum.Intract_500}
-                onSwap={onSwap}
-            />
-
             {IS_TMA ? (
                 <>
                     <PartnerTaskItem
@@ -75,46 +60,6 @@ export const SocialTasks: FC<Props> = ({onSwap}) => {
                         imageSrc={tonAppImage}
                         title="Leave a review"
                         taskType={TaskTypeEnum.TonApp}
-                    />
-
-                    <Divider withArrow={true} className={styles.divider} />
-
-                    <p className={styles.title}>New Year Special ✨</p>
-                    <PartnerTaskItem
-                        isTelegram={true}
-                        imageSrc={blockLabsImage}
-                        title="Open Block Labs"
-                        taskType={TaskTypeEnum.BlockLabs_Bot}
-                    />
-                    <PartnerTaskItem
-                        isTelegram={true}
-                        imageSrc={tonStationImage}
-                        title="Open TON Station"
-                        taskType={TaskTypeEnum.TonStation_Bot}
-                    />
-                    <PartnerTaskItem
-                        isTelegram={true}
-                        imageSrc={claytonImage}
-                        title="Play Clayton Game"
-                        taskType={TaskTypeEnum.Clayton_Bot}
-                    />
-                    <PartnerTaskItem
-                        isTelegram={true}
-                        imageSrc={terminalImage}
-                        title="Open TERMINAL"
-                        taskType={TaskTypeEnum.Terminal_Bot}
-                    />
-                    <PartnerTaskItem
-                        isTelegram={true}
-                        imageSrc={notPixelImage}
-                        title="Draw at Not Pixel"
-                        taskType={TaskTypeEnum.notPixel_Bot}
-                    />
-                    <PartnerTaskItem
-                        isTelegram={true}
-                        imageSrc={appsCenterImage}
-                        title="Open Telegram Apps Center"
-                        taskType={TaskTypeEnum.AppsCenter_Bot}
                     />
 
                     <Divider withArrow={true} className={styles.divider} />
@@ -180,21 +125,6 @@ export const SocialTasks: FC<Props> = ({onSwap}) => {
                         imageSrc={stakingImage}
                         title="Stake any token"
                         taskType={TaskTypeEnum.JVault_Staking}
-                    />
-
-                    <Divider withArrow={true} className={styles.divider} />
-
-                    <TaskHeader name="SnapX" imageSrc={snapxImage} />
-                    <PartnerTaskItem
-                        isTelegram={true}
-                        imageSrc={telegramImage}
-                        title="Join Channel"
-                        taskType={TaskTypeEnum.SnapX_Telegram}
-                    />
-                    <PartnerTaskItem
-                        imageSrc={twitterImage}
-                        title="Follow X"
-                        taskType={TaskTypeEnum.SnapX_Twitter}
                     />
                 </>
             ) : (
